66be78e8fdc12988c4867c647f187b298e01d6d2
Liu <artisan6g@outlook.com>
2018-02-08 01:38:29 +0800

    bug

--- models/lstm.py
+++ models/lstm.py
@@ -9,47 +9,51 @@ from sklearn.metrics import mean_squared_error
 from preprocess.gen_time_series import gen_sale_quantity_series
 # convert an array of values into a dataset matrix
 look_back = 3
-dataset ,testset = gen_sale_quantity_series(look_back=3)
+dataset ,testset = gen_sale_quantity_series(look_back=look_back)
 # fix random seed for reproducibility
 numpy.random.seed(7)
 # load the dataset
 # dataframe = read_csv('international-airline-passengers.csv', usecols=[1], engine='python', skipfooter=3)
-dataset = dataset.values[:][:look_back+1]
+dataset = dataset.values
 numpy.random.shuffle(dataset)
 dataset = dataset.astype('float32')
+X = dataset[:,:look_back]
+Y = dataset[:,:-2]
 # normalize the dataset
 scaler = MinMaxScaler(feature_range=(0, 1))
-dataset = scaler.fit_transform(dataset)
+# X = scaler.fit_transform(X=X,y=Y)
+
 # split into train and test sets
 train_size = int(len(dataset) * 0.67)
 test_size = len(dataset) - train_size
-train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
-# reshape into X=t and Y=t+1
-trainX = train[:][:look_back]
-trainY = train[:][-1]
-testX = test[:][:look_back]
-testY = test[:][:-1]
+trainX, testX = X[0:train_size,:], X[train_size:len(X),:]
+trainY, testY = Y[0:train_size,:], Y[train_size:len(Y),:]
+
+
+
+
+
 # reshape input to be [samples, time steps, features]
-trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
-testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+trainX = numpy.reshape(trainX, (trainX.shape[0], look_back,1))
+testX = numpy.reshape(testX, (testX.shape[0], look_back,1))
 # create and fit the LSTM network
 model = Sequential()
-model.add(LSTM(4, input_shape=(1, look_back)))
+model.add(LSTM(10,input_shape=(look_back,1)))
 model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam')
-model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
+model.fit(trainX, trainY, epochs=10, batch_size=400, verbose=2)
 # make predictions
 trainPredict = model.predict(trainX)
 testPredict = model.predict(testX)
 # invert predictions
-trainPredict = scaler.inverse_transform(trainPredict)
-trainY = scaler.inverse_transform([trainY])
-testPredict = scaler.inverse_transform(testPredict)
-testY = scaler.inverse_transform([testY])
+# trainPredict = scaler.inverse_transform(trainPredict)
+# trainY = scaler.inverse_transform([trainY])
+# testPredict = scaler.inverse_transform(testPredict)
+# testY = scaler.inverse_transform([testY])
 # calculate root mean squared error
-trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+trainScore = math.sqrt(mean_squared_error(trainY[:], trainPredict[:,0]))
 print('Train Score: %.2f RMSE' % (trainScore))
-testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+testScore = math.sqrt(mean_squared_error(testY[:], testPredict[:,0]))
 print('Test Score: %.2f RMSE' % (testScore))
 # shift train predictions for plotting
 trainPredictPlot = numpy.empty_like(dataset)
