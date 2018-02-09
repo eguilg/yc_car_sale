55ed0855a7fd9632586a1858da1f4e26eda68bbe
cqEguil <artisan6g@outlook.com>
2018-02-09 23:05:23 +0800

    除price缺失以外的空值处理，类别特征mapping 统计出共1539个小类 //todo 分析得到小类的时序数据

--- experiments/lstm.py
+++ experiments/lstm.py
@@ -3,18 +3,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 import datetime
-from keras.models import Sequential
+from keras.models import Model
+from keras.layers import Input
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import Flatten
+from keras import backend as K
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import FunctionTransformer
 from sklearn.metrics import mean_squared_error
 from preprocess.gen_time_series import gen_sale_quantity_series
 # convert an array of values into a dataset matrix
 seq_len = 12
-k = 1
+
 epoch = 500
-batch_size = 200
+batch_size = 300
 dataset ,testset = gen_sale_quantity_series(seq_len=seq_len)
 # fix random seed for reproducibility
 np.random.seed(7)
@@ -28,10 +31,11 @@ Y = dataset[:,-2]
 # normalize the dataset
 scalerX = MinMaxScaler(feature_range=(0, 1))
 scalerY = MinMaxScaler(feature_range=(0, 1))
+
 X = scalerX.fit_transform(X)
 Y = scalerY.fit_transform(np.reshape(Y,(-1,1)))
 # split into train and vali sets
-train_size = int(len(dataset) * 0.67)
+train_size = int(len(dataset) * 0.80)
 vali_size = len(dataset) - train_size
 trainX, valiX = X[0:train_size,:], X[train_size:len(X),:]
 trainY, valiY = Y[0:train_size,:], Y[train_size:len(Y),:]
@@ -41,33 +45,32 @@ trainY, valiY = Y[0:train_size,:], Y[train_size:len(Y),:]
 trainX = np.reshape(trainX, (trainX.shape[0], seq_len,1))
 valiX = np.reshape(valiX, (valiX.shape[0], seq_len,1))
 # create and fit the LSTM network
-model = Sequential()
-model.add(LSTM(32,input_shape=(trainX.shape[1],trainX.shape[2]),
-               dropout_W=0.2, dropout_U=0.2))
 
-model.add(Dense(trainY.shape[1]))
+seq_input = Input(shape=(trainX.shape[1],trainX.shape[2]), dtype='float32', name='seq_input')
+lstm_out = LSTM(32,input_shape=(trainX.shape[1],trainX.shape[2]),
+               dropout_W=0.2, dropout_U=0.2)(seq_input)
+main_out = Dense(trainY.shape[1],name='main_out')(lstm_out)
+
+model = Model(inputs=[seq_input ], outputs=[main_out])
 
 model.compile(loss='mean_squared_error', optimizer='adam')
 model.summary()
-model.fit(trainX, trainY, epochs=epoch, batch_size=batch_size, verbose=2)
+model.fit([trainX], [trainY], epochs=epoch, batch_size=batch_size, verbose=2)
 # make predictions
 trainPredict = model.predict(trainX)
 valiPredict = model.predict(valiX)
-# invert predictions
-# trainPredict = scaler.inverse_transform(trainPredict)
-# trainY = scaler.inverse_transform([trainY])
-# valiPredict = scaler.inverse_transform(valiPredict)
-# valiY = scaler.inverse_transform([valiY])
-# calculate root mean squared error
+
 totallY = np.vstack((trainPredict,valiPredict))
-inversedY = k*scalerY.inverse_transform(totallY)
+inversedY = scalerY.inverse_transform(totallY)
+
 trainPredict,valiPredict = inversedY[0:train_size,:], inversedY[train_size:len(inversedY),:]
 Y = scalerY.inverse_transform(Y)
+
 trainY, valiY = Y[0:train_size,:], Y[train_size:len(Y),:]
 trainScore = math.sqrt(mean_squared_error(trainY[:], trainPredict[:,0]))
-print('Train Score: %.2f mse' % (trainScore))
+print('Train Score: %.2f rmse' % (trainScore))
 valiScore = math.sqrt(mean_squared_error(valiY[:], valiPredict[:,0]))
-print('vali Score: %.2f mse' % (valiScore))
+print('vali Score: %.2f rmse' % (valiScore))
 # shift train predictions for plotting
 fig = plt.figure()
 ax1=fig.add_subplot(211)
@@ -88,7 +91,8 @@ testX = testset.values[:,:seq_len]
 testX = np.reshape(scalerX.transform(testX),(len(testX),seq_len,1))
 
 testPredict = model.predict(testX)
-testPredict = k*scalerY.inverse_transform(testPredict)
+testPredict = scalerY.inverse_transform(testPredict)
+
 sub = pd.read_csv('../data/yancheng_testA_20171225.csv')
 sub.predict_quantity = np.reshape(testPredict,(testPredict.shape[0]))
 timestamp = datetime.datetime.now().strftime('%m%d%H%M')
--- util/load_data.py
+++ util/load_data.py
@@ -1,9 +1,73 @@
 import pandas as pd
 import numpy as np
+import datetime
+
+def price_lower(price_level):
+    price_level = price_level.replace('WL','')
+    price_level = price_level.replace('W', '')
+    boundary= price_level.split('-')
+    if(len(boundary)==1):
+        return 0
+    else:
+        return int(boundary[0])
+
+def price_upper(price_level):
+    price_level = price_level.replace('WL', '')
+    price_level = price_level.replace('W', '')
+    boundary = price_level.split('-')
+    if (len(boundary) == 1):
+        return  int(boundary[0])
+    else:
+        return  int(boundary[1])
 
 def load_raw_data(path='../data/yancheng_train.csv'):
 
-    parser = lambda date: pd.datetime.strptime(date, '%Y%m')
-    train_data = pd.read_csv(path, parse_dates=['sale_date'], date_parser=parser)
-    train_data.sort_values(by=['sale_date'], inplace=True)
+    train_data = pd.read_csv('../data/yancheng_train.csv', na_values=['-'])
+    train_data.sale_date = pd.to_datetime(train_data.sale_date, format = '%Y%m')
+
+    train_data['year'] = train_data.sale_date.apply(lambda x:x.year)
+    train_data['month'] = train_data.sale_date.apply(lambda x:x.month)
+    train_data['time_index'] = (train_data.year-2012)*12+train_data.month
+    train_data.level_id.fillna(0, inplace=True)
+    train_data.level_id=train_data.level_id.astype(int)
+    train_data.loc[train_data.power=='81/70','power']=81
+    train_data.power = train_data.power.astype(float)
+    train_data.loc[train_data.engine_torque == '155/140','engine_torque'] = 155
+    train_data.engine_torque.fillna(100,inplace=True)
+    train_data.engine_torque = train_data.engine_torque.astype(float)
+    train_data.fuel_type_id.fillna(1, inplace=True)
+    train_data.fuel_type_id = train_data.fuel_type_id.astype(int)
+
+    train_data.TR = train_data.TR.map({'0':0,
+                                       '1':1,
+                                       '4':4,
+                                       '5;4':4.5,
+                                       '5':5,
+                                       '6':6,
+                                       '7':7,
+                                       '8;7':7.5,
+                                       '8':8,
+                                       '9':9}).astype(float)
+    train_data.gearbox_type = train_data.gearbox_type.map({'AMT':0,
+                                                           'AT':1,
+                                                           'AT;DCT':2,
+                                                           'CVT':3,
+                                                           'DCT':4,
+                                                           'MT':5,
+                                                           'MT;AT':6}).astype(int)
+    train_data.if_charging = train_data.if_charging.map({'L':0,'T':1})
+    train_data.rated_passenger = train_data.rated_passenger.map({'4':4,
+                                                                 '5':5,
+                                                                 '4-5':4.5,
+                                                                 '5-7':6.1,
+                                                                 '5-8':6.6,
+                                                                 '6-7':6.5,
+                                                                 '7':7,
+                                                                 '6-8':7.1,
+                                                                 '7-8':7.5,
+                                                                 '9':9}).astype(float)
+    train_data['price_lower'] = train_data.price_level.apply(price_lower)
+    train_data['price_upper'] = train_data.price_level.apply(price_upper)
+    train_data.drop('price_level',axis=1,inplace=True)
+    train_data.sort_values(by=['class_id', 'time_index'], inplace=True)
     return train_data
