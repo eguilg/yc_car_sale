b3243f6c932959ca2a8666fb31a0d4794a605e61
root <root@munny.cloud.mos>
2018-02-20 08:01:49 -0500

    still 105

--- lstm.py
+++ lstm.py
@@ -3,7 +3,7 @@ import pandas as pd
 import datetime
 import keras
 from keras.models import Model
-from keras.layers import Input,Dense,LSTM,Masking,Merge,Dropout
+from keras.layers import Input,Dense,GRU,LSTM,Masking,Merge,Dropout
 from keras.optimizers import Adam, SGD
 from keras.callbacks import ReduceLROnPlateau,EarlyStopping,CSVLogger
 from keras import backend as K
@@ -18,7 +18,7 @@ def rmse_loss(ground_truth, pred):
     return K.sqrt(K.mean(K.square(ground_truth-pred),axis=-1))
 
 def create_model(dense_shape,year_seq_shape,month_seq_shape,
-                 seq_size = 64, final_dense_size = 32,
+                 seq_size = 100, final_dense_size = 32,
                  lr = 0.1, decay = 1e-5):
 
     class_dense_input = Input(shape=dense_shape, name='dense_input')
@@ -27,21 +27,21 @@ def create_model(dense_shape,year_seq_shape,month_seq_shape,
     year_seq_input = Input(shape=year_seq_shape, name='year_seq_input')
     year_seq_mask = Masking(mask_value=-1, input_shape=year_seq_shape,
                             name='year_seq_mask')(year_seq_input)
-    year_seq_out = LSTM(seq_size, input_shape=year_seq_shape,
+    year_seq_out = GRU(seq_size, input_shape=year_seq_shape,
                         dropout_W=0.1, dropout_U=0.1, name='year_seq')(year_seq_mask)
 
     month_seq_input = Input(shape=month_seq_shape, name='month_seq_input')
     month_seq_mask = Masking(mask_value=-1, input_shape=month_seq_shape,
                              name='month_seq_mask')(month_seq_input)
-    month_seq_out = LSTM(seq_size, input_shape=month_seq_shape,
+    month_seq_out = GRU(seq_size, input_shape=month_seq_shape,
                          dropout_W=0.1, dropout_U=0.1, name='month_seq')(month_seq_mask)
 
     seq_merge = Merge(name='seq_merge', mode = 'ave')([year_seq_out, month_seq_out])
     final_merge = Merge(name='final_merge',mode='concat')([class_dense_out, seq_merge])
-
-    final_dense = Dense(final_dense_size,name='final_dense')(final_merge)
-    drop_out = Dropout(0.1)(final_dense)
-    main_out = Dense(1,name='main_out',activation='softplus')(drop_out)
+    dense_after_merge = Dense(seq_size, name = 'dense_after_merge')(final_merge)
+    drop_out = Dropout(0.1, name = 'drop_out')(dense_after_merge)
+    final_dense = Dense(final_dense_size,name='final_dense')(drop_out)
+    main_out = Dense(1,name='main_out',activation='softplus')(final_dense)
 
     model = Model(inputs=[class_dense_input, year_seq_input, month_seq_input],
                   outputs=[main_out])
